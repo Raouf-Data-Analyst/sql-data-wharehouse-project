ab22bc3c56d224862c2dc8cdf8cd99c171886f40
Abderaouf_LAROUCI <99683777+Raouf-Data-Analyst@users.noreply.github.com>
2025-05-21 16:06:19 +0200

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,221 +1,98 @@
-/* ===========================================================================================
-   Script SQL – Création et chargement des tables Silver (zone de traitement) pour CRM & ERP
-   -------------------------------------------------------------------------------------------
-   Description :
-   - Supprime et recrée les tables Silver : crm_cust_info, crm_prd_info, crm_sales_details,
-     erp_cust_az12, erp_loc_a101, erp_px_cat_g1v2.
-   - Charge les données depuis les fichiers CSV situés dans les répertoires source_crm et 
-     source_erp à l’aide de BULK INSERT après avoir vidé les tables (TRUNCATE).
-   - Préparation pour le traitement et la transformation dans un data warehouse SQL.
-   
-   Remarques :
-   - Chaque table est vérifiée (DROP si elle existe), puis recréée.
-   - Les insertions se font en mode full load (TRUNCATE + BULK INSERT).
-   - Les chemins de fichiers doivent être accessibles et les droits de lecture accordés.
-=========================================================================================== */
+/*
+===============================================================================
+DDL Script: Create Silver Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 
---TSQL check
+IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_cust_info;
+GO
 
-IF OBJECT_ID('silver.crm_cust_info','U') IS NOT NULL
-	DROP TABLE silver.crm_cust_info
-
-
-CREATE TABLE silver.crm_cust_info
-(
-    cst_id INT ,
-    cst_key NVARCHAR(50),
-    cst_firstname NVARCHAR(50) ,
-    cst_lastname NVARCHAR(50) ,
-    cst_marital_status NVARCHAR(20),
-    cst_gndr NVARCHAR(50),
-    cst_create_date DATETIME 
+CREATE TABLE silver.crm_cust_info (
+    cst_id             INT,
+    cst_key            NVARCHAR(50),
+    cst_firstname      NVARCHAR(50),
+    cst_lastname       NVARCHAR(50),
+    cst_marital_status NVARCHAR(50),
+    cst_gndr           NVARCHAR(50),
+    cst_create_date    DATE,
+    dwh_create_date    DATETIME2 DEFAULT GETDATE()
 );
+GO
 
---TSQL check 
-IF OBJECT_ID('silver.crm_prd_info','U') IS NOT NULL
-	DROP TABLE silver.crm_prd_info
+IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_prd_info;
+GO
 
-CREATE TABLE silver.crm_prd_info
-(
-    prd_id INT,
-    prd_key NVARCHAR(50) ,
-    prd_nm NVARCHAR(50) ,
-    prd_cost INT,
-    prd_line NVARCHAR(50),
-    prd_start_dt DATETIME ,
-    prd_end_dt DATETIME
-
-)
-
---TSQL check 
-IF OBJECT_ID('silver.crm_sales_details','U') IS NOT NULL
-	DROP TABLE silver.crm_sales_details
-
-CREATE TABLE silver.crm_sales_details
-(
-    sls_ord_num NVARCHAR(50) ,
-    sls_prd_key NVARCHAR(50) ,
-    sls_cust_id INT ,
-    sls_order_dt INT ,
-    sls_ship_dt INT,
-    sls_due_dt INT,
-    sls_sales INT,
-    sls_quantity INT,
-    sls_price INT 
+CREATE TABLE silver.crm_prd_info (
+    prd_id          INT,
+    cat_id          NVARCHAR(50),
+    prd_key         NVARCHAR(50),
+    prd_nm          NVARCHAR(50),
+    prd_cost        INT,
+    prd_line        NVARCHAR(50),
+    prd_start_dt    DATE,
+    prd_end_dt      DATE,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
+GO
 
+IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE silver.crm_sales_details;
+GO
 
---CREATE Tables from ERP
-
---TSQL check 
-IF OBJECT_ID('silver.erp_cust_az12','U') IS NOT NULL
-	DROP TABLE silver.erp_cust_az12
-
-CREATE TABLE silver.erp_cust_az12
-(
-    cid NVARCHAR(50),
-    bdate DATETIME,
-    gen NVARCHAR(50)
+CREATE TABLE silver.crm_sales_details (
+    sls_ord_num     NVARCHAR(50),
+    sls_prd_key     NVARCHAR(50),
+    sls_cust_id     INT,
+    sls_order_dt    DATE,
+    sls_ship_dt     DATE,
+    sls_due_dt      DATE,
+    sls_sales       INT,
+    sls_quantity    INT,
+    sls_price       INT,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
+GO
 
-IF OBJECT_ID('silver.erp_loc_a101','U') IS NOT NULL
-	DROP TABLE silver.erp_loc_a101
+IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE silver.erp_loc_a101;
+GO
 
-CREATE TABLE silver.erp_loc_a101
-(
-    cid NVARCHAR(50) ,
-	cntry NVARCHAR(50)
+CREATE TABLE silver.erp_loc_a101 (
+    cid             NVARCHAR(50),
+    cntry           NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
+GO
 
-IF OBJECT_ID('silver.erp_px_cat_g1v2','U') IS NOT NULL
-	DROP TABLE silver.erp_px_cat_g1v2
+IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE silver.erp_cust_az12;
+GO
 
-CREATE TABLE silver.erp_px_cat_g1v2
-(
-    id NVARCHAR(50),
-	cat NVARCHAR(50),
-	subcat NVARCHAR(50),
-	maintenance NVARCHAR(50)
+CREATE TABLE silver.erp_cust_az12 (
+    cid             NVARCHAR(50),
+    bdate           DATE,
+    gen             NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
+GO
 
---Load data from csv to tables 
+IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE silver.erp_px_cat_g1v2;
+GO
 
--- Before laoding we do TRUNCATE ( Make table empty and than do the Fulllaod) 
-
---===========================================================
--- TRUNCATE and LOAD DATA INTO CRM TABLES
---===========================================================
-
--- TRUNCATE crm_cust_info
-TRUNCATE TABLE silver.crm_cust_info;
-
--- BULK INSERT into crm_cust_info
--- Loads customer information data
-BULK INSERT silver.crm_cust_info
-FROM 'D:\15 - Cours SQL\01- sql-data-warehouse -Project\datasets\source_crm\cust_info.csv'
-WITH 
-(
-    FIRSTROW = 2,   -- Skips the header row
-    FIELDTERMINATOR = ',',   -- Specifies comma as the delimiter
-    TABLOCK   -- Optimizes the bulk load
+CREATE TABLE silver.erp_px_cat_g1v2 (
+    id              NVARCHAR(50),
+    cat             NVARCHAR(50),
+    subcat          NVARCHAR(50),
+    maintenance     NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
+GO
 
-
--- TRUNCATE crm_prd_info
-TRUNCATE TABLE silver.crm_prd_info;
-
--- BULK INSERT into crm_prod_info
--- Loads product information data
-BULK INSERT silver.crm_prd_info
-FROM 'D:\15 - Cours SQL\01- sql-data-warehouse -Project\datasets\source_crm\prd_info.csv'
-WITH 
-(
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
-
-
--- TRUNCATE crm_sales_details
-TRUNCATE TABLE silver.crm_sales_details;
-
--- BULK INSERT into crm_sales_details
--- Loads sales details data
-BULK INSERT silver.crm_sales_details
-FROM 'D:\15 - Cours SQL\01- sql-data-warehouse -Project\datasets\source_crm\sales_details.csv'
-WITH 
-(
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
-
-
---===========================================================
--- TRUNCATE and LOAD DATA INTO CRM TABLES
---===========================================================
--- TRUNCATE crm_sales_details
-TRUNCATE TABLE silver.crm_sales_details;
-
--- BULK INSERT into crm_sales_details
-BULK INSERT silver.crm_sales_details
-FROM 'D:\15 - Cours SQL\01- sql-data-warehouse -Project\datasets\source_crm\sales_details.csv'
-WITH 
-(
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
-
-
-BULK INSERT silver.crm_sales_details
-FROM 'D:\15 - Cours SQL\01- sql-data-warehouse -Project\datasets\source_crm\sales_details.csv'
-WITH 
-(
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
-
---===========================================================
--- TRUNCATE and LOAD DATA INTO ERP TABLES
---===========================================================
-
--- TRUNCATE erp_cust_az12
-TRUNCATE TABLE silver.erp_cust_az12;
-
--- BULK INSERT into erp_cust_az12
-BULK INSERT silver.erp_cust_az12
-FROM 'D:\15 - Cours SQL\01- sql-data-warehouse -Project\datasets\source_erp\cust_az12.csv'
-WITH 
-(
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
-
--- TRUNCATE erp_loc_a101
-TRUNCATE TABLE silver.erp_loc_a101;
-
--- BULK INSERT into erp_loc_a101
-BULK INSERT silver.erp_loc_a101
-FROM 'D:\15 - Cours SQL\01- sql-data-warehouse -Project\datasets\source_erp\loc_a101.csv'
-WITH 
-(
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
-
--- TRUNCATE erp_px_cat_g1v2
-TRUNCATE TABLE silver.erp_px_cat_g1v2;
-
--- BULK INSERT into erp_px_cat_g1v2
-BULK INSERT silver.erp_px_cat_g1v2
-FROM 'D:\15 - Cours SQL\01- sql-data-warehouse -Project\datasets\source_erp\px_cat_g1v2.csv'
-WITH 
-(
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
